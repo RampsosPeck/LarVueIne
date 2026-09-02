96c3b2d029f7677ac4c4637e77331a106b1f1890
RampsosPeck <peralta.jorge.uatf@gmail.com>
2026-09-02 15:00:07 -0400

    Config init

---
+++ resources/js/app.ts
@@ -0,0 +1,11 @@
+import { createInertiaApp } from '@inertiajs/vue3';
+import './bootstrap';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+void createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    progress: {
+        color: '#4B5563',
+    },
+});
